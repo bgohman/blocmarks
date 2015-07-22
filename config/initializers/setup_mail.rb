c1b1f7681a02b419ef6d1540836433a3e0e72334
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-07-22 09:26:20 -0500

    allow devise to accept name

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -12,5 +12,19 @@ if Rails.env.development? || Rails.env.production?
   }
   ActionMailer::Base.raise_delivery_errors = true
 
+  # This interceptor just makes sure that local mail only emails you.
+  # http://edgeguides.rubyonrails.org/action_mailer_basics.html#intercepting-emails
+  class DevelopmentMailInterceptor
+    def self.delivering_email(message)
+      message.to =  'bradley.s.gohman@gmail.com'
+      message.cc = nil
+      message.bcc = nil
+    end
+  end
 
+  # Locally, outgoing mail will be 'intercepted' by the
+  # above DevelopmentMailInterceptor before going out
+  if Rails.env.development?
+    ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
+  end
 end
